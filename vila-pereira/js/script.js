6f9ac4cd574f4e9848eda539c5aa3306b6df0671
yrnsaraiva <85243493+yrnsaraiva@users.noreply.github.com>
2022-08-01 15:09:01 +0200

    Add files via upload

---
+++ vila-pereira/js/script.js
@@ -0,0 +1,32 @@
+//Scroll reveal
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.animate-left', {
+	origin: 'left',
+	duration: 1000,
+	distance: '25rem',
+	delay: 100
+
+});
+
+sr.reveal('.animate-right',  {
+	origin: 'right',
+	duration: 1000,
+	distance: '25rem',
+	delay: 100
+
+});
+
+sr.reveal('.animate-bottom',  {
+	origin: 'bottom',
+	duration: 2000,
+	distance: '25rem',
+	delay: 100
+});
+
+sr.reveal('.animate-top',  {
+	origin: 'top',
+	duration: 2000,
+	distance: '25rem',
+	delay: 100
+});
